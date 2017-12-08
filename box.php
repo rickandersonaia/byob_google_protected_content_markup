f0b099a0e0c4e2333ac179af7db8b42e0d50368d
Rick Anderson <rick@byobwebsite.com>
2017-12-08 05:54:38 -0800

    working correctly

--- box.php
+++ box.php
@@ -64,6 +64,8 @@ class byob_google_protected_content_markup extends thesis_box {
 				$byob_ah = new byob_asset_handler;
 			}
 		}
+		add_filter( 'the_content', array( $this, 'more_tag_protection' ), 1 );
+		add_action( 'wp_footer', array( $this, 'JSON_LD_markup' ) );
 	}
 
 	/**
@@ -74,81 +76,204 @@ class byob_google_protected_content_markup extends thesis_box {
 			'title'  => __( 'Protected Content Markup', 'byobgpcm' ),
 			'fields' => array(
 				'use_protected_content_markup' => array(
-					'type'    => 'checkbox',
-					'options' => array(
+					'type'       => 'checkbox',
+					'label'      => __( 'Does this template contain protected content?', 'byobgpcm' ),
+					'options'    => array(
 						'yes' => __( 'Use protected content markup on this template', 'byobgpcm' )
 					),
-					'dependents' => array('yes')
+					'dependents' => array( 'yes' )
 				),
-				'use_more_protection' => array(
+				'organization_name'                    => array(
+					'type'        => 'text',
+					'label'       => __( 'Organization name is required', 'byobgpcm' ),
+					'width' => 'full',
+					'placeholder' => 'ACME Company',
+					'parent'      => array(
+						'use_protected_content_markup' => 'yes'
+					),
+					'tooltip'     => __( 'Google requires that the name of the organization be published', 'byobgpcm' )
+				),
+				'logo_url'                    => array(
+					'type'        => 'text',
+					'label'       => __( 'Organization logo URL', 'byobgpcm' ),
+					'width' => 'full',
+					'placeholder' => 'http://acmeco.com/logo-img.jpg',
+					'parent'      => array(
+						'use_protected_content_markup' => 'yes'
+					),
+					'tooltip'     => __( 'Google requires a logo image for the organization - use an absolute url', 'byobgpcm' )
+				),
+				'use_more_protection'          => array(
 					'type'    => 'checkbox',
+					'label'   => __( 'Does this template protect content using the "More Tag"?', 'byobgpcm' ),
 					'options' => array(
 						'yes' => __( 'Include "More Tag" protection', 'byobgpcm' )
 					),
-					'parent' => array(
-						'use_protected_content_markup' => 'yes')
+					'parent'  => array(
+						'use_protected_content_markup' => 'yes'
+					)
 				),
-				'selector1' => array(
-					'type'    => 'text',
-					'width' => 'medium',
-					'code' => 'true',
+				'selector1'                    => array(
+					'type'        => 'text',
+					'label'       => __( 'Selector to protect', 'byobgpcm' ),
+					'width'       => 'medium',
+					'code'        => 'true',
 					'placeholder' => '.post_box',
-					'parent' => array(
-						'use_protected_content_markup' => 'yes'),
-					'tooltip' => __( 'place the full selector you wish to be marked as protected.  Include the class and or ID indicator', 'byobgpcm' )
+					'parent'      => array(
+						'use_protected_content_markup' => 'yes'
+					),
+					'tooltip'     => __( 'place the full selector you wish to be marked as protected.  Include the class and or ID indicator', 'byobgpcm' )
 				),
-				'selector2' => array(
-					'type'    => 'text',
-					'width' => 'medium',
-					'code' => 'true',
+				'selector2'                    => array(
+					'type'        => 'text',
+					'label'       => __( 'Selector to protect', 'byobgpcm' ),
+					'width'       => 'medium',
+					'code'        => 'true',
 					'placeholder' => '.comment_list',
-					'parent' => array(
-						'use_protected_content_markup' => 'yes'),
-					'tooltip' => __( 'place the full selector you wish to be marked as protected.  Include the class and or ID indicator', 'byobgpcm' )
+					'parent'      => array(
+						'use_protected_content_markup' => 'yes'
+					),
+					'tooltip'     => __( 'place the full selector you wish to be marked as protected.  Include the class and or ID indicator', 'byobgpcm' )
 				),
-				'selector3' => array(
-					'type'    => 'text',
-					'width' => 'medium',
-					'code' => 'true',
+				'selector3'                    => array(
+					'type'        => 'text',
+					'label'       => __( 'Selector to protect', 'byobgpcm' ),
+					'width'       => 'medium',
+					'code'        => 'true',
 					'placeholder' => '#commentform',
-					'parent' => array(
-						'use_protected_content_markup' => 'yes'),
-					'tooltip' => __( 'place the full selector you wish to be marked as protected.  Include the class and or ID indicator', 'byobgpcm' )
+					'parent'      => array(
+						'use_protected_content_markup' => 'yes'
+					),
+					'tooltip'     => __( 'place the full selector you wish to be marked as protected.  Include the class and or ID indicator', 'byobgpcm' )
 				),
-				'selector4' => array(
-					'type'    => 'text',
-					'width' => 'medium',
-					'code' => 'true',
+				'selector4'                    => array(
+					'type'        => 'text',
+					'label'       => __( 'Selector to protect', 'byobgpcm' ),
+					'width'       => 'medium',
+					'code'        => 'true',
 					'placeholder' => '.post_box',
-					'parent' => array(
-						'use_protected_content_markup' => 'yes'),
-					'tooltip' => __( 'place the full selector you wish to be marked as protected.  Include the class and or ID indicator', 'byobgpcm' )
+					'parent'      => array(
+						'use_protected_content_markup' => 'yes'
+					),
+					'tooltip'     => __( 'place the full selector you wish to be marked as protected.  Include the class and or ID indicator', 'byobgpcm' )
 				)
 			)
 		);
 	}
-	/**
-	 * @param array $args - a variable containing $depth and other potential data
-	 *
-	 * Box API Method of outputting HTML at the location of the box in the template
-	 * Typically echo'd rather than returned
-	 */
-	public function html( $args = array() ) {
-		global $thesis;
-		extract( $args = is_array( $args ) ? $args : array() );
-		$depth   = isset( $depth ) ? $depth : 0;
-		$tab     = str_repeat( "\t", $depth );
-		$html    = ! empty( $this->options['html'] ) ? trim( esc_attr( $this->options['html'] ) ) : 'div';
-		$class   = ! empty( $this->options['class'] ) ? ' class="' . trim( esc_attr( $this->options['class'] ) ) . '"' : '';
-		$id      = ( ! empty( $this->options['id'] ) ? ' id="' . trim( esc_attr( $this->options['id'] ) ) . '"' : '' );
-		$message = ! empty( $this->options['message'] ) ? trim( wp_kses_post( $this->options['message'] ) ) : false;
 
-		echo "$tab<$html$id$class>\n";
-			if ( $message ) {
-				echo $message;
+	public function more_tag_protection( $content ) {
+		global $post;
+		if ( isset( $this->template_options['use_protected_content_markup']['yes'] ) &&
+		     isset( $this->template_options['use_more_protection']['yes'] ) ) {
+
+			$unfiltered_content = $post->post_content;
+			$content_fragments  = explode( '-->', $unfiltered_content, 2 );
+
+			if ( $content_fragments ) {
+				$first_half  = strval( $content_fragments[0] ) . '-->';
+				$second_half = '<div class="paywall">' . $content_fragments[1] . '</div>';
+
+				return $first_half . $second_half;
+
+			} else {
+				return $content;
 			}
-			echo $this->rotator(array_merge($args, array('depth' => $depth + 1)));
-		echo "$tab</$html>\n";
+
+		} else {
+			return $content;
+		}
+	}
+
+	public function JSON_LD_markup() {
+		global $post;
+		$post_image_url = $this->select_post_image( $post->ID );
+		$author_name    = get_the_author_meta( 'display_name', $post->post_author );
+		$org_name = !empty($this->template_options['organization_name']) ? esc_html($this->template_options['organization_name']) : "";
+		$logo_url = !empty($this->template_options['logo_url']) ? esc_url($this->template_options['logo_url']) : "";
+
+		if ( isset( $this->template_options['use_protected_content_markup']['yes'] ) ) {
+			?>
+			<script type="application/ld+json">
+				{
+				  "@context": "http://schema.org",
+				  "@type": "Article",
+				  "mainEntityOfPage": {
+				    "@type": "WebPage",
+				    "@id": "<?php echo get_the_permalink( $post->ID ) ?>"
+				  },
+				  "headline": "<?php echo get_the_title( $post->ID ) ?>",
+				  <?php if ( $post_image_url ) {
+				echo '"image": "' . $post_image_url . '",' . "\n";
+				?>
+				  "datePublished": "<?php echo $post->post_date; ?>",
+				  "dateModified": "<?php echo $post->post_modified; ?>",
+				  "author": {
+				    "@type": "Person",
+				    "name": "<?php echo $author_name; ?>"
+				  },
+				  "publisher": {
+				     "name": "<?php echo $org_name; ?>",
+				     "@type": "Organization",
+				     "logo": {
+				        "@type": "ImageObject",
+				        "url": "<?php echo $logo_url; ?>"
+				     }
+				  },
+				  "description": "<?php echo get_the_excerpt( $post->ID ) ?>",
+				  "isAccessibleForFree": "False",
+				  "hasPart":<?php echo $this->build_the_protected_selector_array(); ?>
+				}
+
+
+
+			</script>
+			<?php
+		}
+		}
+	}
+
+	public function build_the_protected_selector_array() {
+		$selectors       = array( 'selector1', 'selector2', 'selector3', 'selector4' );
+		$custom_selector = "";
+		$t               = "\t\t\t\t\t";
+
+		$given_selector = "$t{\n";
+		$given_selector .= "$t\"@type\": \"WebPageElement\",\n";
+		$given_selector .= "$t\"isAccessibleForFree\": \"False\",\n";
+		$given_selector .= "$t\"cssSelector\" : \".paywall\"\n";
+		$given_selector .= "$t}\n";
+
+		foreach ( $selectors as $selector ) {
+			if ( ! empty( $this->template_options[ $selector ] ) ) {
+				$custom_selector .= "$t{\n";
+				$custom_selector .= "$t\"@type\": \"WebPageElement\",\n";
+				$custom_selector .= "$t\"isAccessibleForFree\": \"False\",\n";
+				$custom_selector .= "$t\"cssSelector\" : \"" . esc_attr( $this->template_options[ $selector ] ) . "\"\n";
+				$custom_selector .= "$t},\n";
+			}
+		}
+		if ( ! empty( $custom_selector ) ) {
+			$selector_array = "[\n" . $custom_selector . $given_selector . "$t]\n";
+		} else {
+			$selector_array = "\n" . $given_selector;
+		}
+
+		return $selector_array;
+	}
+
+	public function select_post_image( $post_id ) {
+		$post_image = get_the_post_thumbnail_url( $post_id );
+		if ( $post_image ) {
+			return $post_image;
+		}
+
+		$post_images = get_attached_media( 'image', $post_id );
+		$post_image  = $post_images[0];
+		if ( ! empty( $post_image->ID ) ) {
+			return wp_get_attachment_image_src( $post_image->ID, 'full' );
+		}
+
+		return false;
 	}
 
 }
